c3467b4906b25971621847ce1809182076965765
Sahil Yadav <raosahil8290@gmail.com>
2026-07-29 01:09:27 +0530

    feat: initialize React root with theme configuration and i18n support in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './i18n'
 import App from './App.jsx'
+import { useThemeStore } from './store/useThemeStore'
+
+useThemeStore.getState().initTheme()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
